2eecb35d9cbca5562a5ac0d212a6d6de536dcfee
Artenes Nogueira <artenesama@gmail.com>
2019-09-27 20:05:53 -0400

    chore: creates Employee model

---
+++ src/schemas/Employee.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const profileSchema = new mongoose.Schema(
+  {
+    email: String,
+    password: String,
+  },
+  { _id: false }
+);
+
+const contractSchema = new mongoose.Schema(
+  {
+    daily_hours: String,
+    lunch_hours: String,
+  },
+  { _id: false }
+);
+
+const justifcationSchema = new mongoose.Schema(
+  {
+    hours: String,
+    reason: String,
+  },
+  { _id: false }
+);
+
+const workdaySchema = new mongoose.Schema({
+  date: String,
+  clocks: [{ hour: String, reason: String }],
+  justification: justifcationSchema,
+  absent: Boolean,
+});
+
+const balanceSchema = new mongoose.Schema(
+  {
+    base: String,
+  },
+  { timestamps: true }
+);
+
+const employeeSchema = new mongoose.Schema({
+  profile: profileSchema,
+  balance: balanceSchema,
+  contract: contractSchema,
+  workdays: [workdaySchema],
+});
+
+export default mongoose.model('Employee', employeeSchema);
